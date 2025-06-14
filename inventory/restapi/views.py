b3979e67161672138f68e0375a2b635d7146af7a
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-14 09:33:14 +0530

    Create And Get Endpoints For Items Created

--- inventory/restapi/views.py
+++ inventory/restapi/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from restapi.serializers import ItemSerializer
+from restapi.models import Item
+class ItemCR(APIView):
+    def post(self,request):
+        try:
+            serializer = ItemSerializer(data=request.data)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        except:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def get(self,request):
+        items = Item.objects.all()
+        serialized_items = ItemSerializer(data=items)
+        return Response(serialized_items.data,status=status.HTTP_200_OK)
